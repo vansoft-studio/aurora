b40ea101d18a4af0fd1eaf8e0bebcf86aa64bad1
mushvan <Mushthaque@vansoft.studio>
2019-08-14 11:21:39 +0530

    image slider page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Grid from '@material-ui/core/Grid';
-import Slideshow from './component/text';
+import React from 'react'
+import './App.css'
+import Grid from '@material-ui/core/Grid'
+import Slideshow from './component/text'
 
-
-function App() {
+function App () {
   return (
-   <Grid>
-      <Slideshow/>
+    <Grid>
+      <Slideshow />
     </Grid>
-  );
+  )
 }
 
-export default App;
+export default App
