2d50b1fa17add109979623455d0e1987467c3d42
Anaghasooraj <52409680+Anaghasooraj@users.noreply.github.com>
2019-08-16 12:31:09 +0530

    Update background_video.js

--- src/components/background_video.js
+++ src/components/background_video.js
@@ -5,22 +5,11 @@ import '../App.css';
 import Grid from '@material-ui/core/Grid';
 import 'react-animated-slider/build/horizontal.css'
 import Slider from 'react-animated-slider'
-
-
-
-
 class Background extends Component { 
-
   render() {
-
     return ( 
-      
       <Grid container xs={12} className="logo" >
-        
-
-      
        <div classNamexs={12} style={{ width: '100%', height: window.innerHeight, position: 'relative'}}>
-        
         <Grid className='video-background' >
           <video id="bg_video" autoPlay muted
             src={require(`../images/city_video.mp4`)}
@@ -33,37 +22,21 @@ class Background extends Component {
                 <h1>Video backgronds are cool..</h1>
                 <p>Video backgronds are a great way of conveying key information about your buisness.</p>
                 <button>Curious Customers click here</button>
-                
-              </div>
-             
+                </div>
             </div>
-           
-
             <div key='2' className="slider-content" >
               <div className="inner">
                 <h1>Create a first impression</h1>
                 <p>Or simply create a great first impression with your Customers</p>
                 <button>Customer is in love with you</button>
-                
               </div>
-              
             </div>
             <Grid XS={12} style={{ position:'absolute',top:'0', left: '0',  zIndex:'10+00', width:'100%', height:'100%' }}/>
-           
-          
           </Slider>
-          
-
-        
-       
         </Grid>
-      
-    
       </div>
       </Grid>
-     
     )
-    
   }
 }
 export default Background
